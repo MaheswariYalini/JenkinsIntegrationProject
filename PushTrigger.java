f1c4172ec672f50c584df9cb1a0c56ae5912e9f8
MaheswariYalini <113038235+MaheswariYalini@users.noreply.github.com>
2022-10-05 12:50:58 -0400

    Update PushTrigger.java

--- PushTrigger.java
+++ PushTrigger.java
@@ -4,5 +4,6 @@ public static void main(String[] args){
 
 System.out.println("Check integration of GIT and Jenkins");
 System.out.println("Check Build is triggered after the GIT Commit");
+  System.out.println("This is after the second commit")
 }
 }
