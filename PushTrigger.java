76d6e65937c9085569702ea12d4be820b7921628
MaheswariYalini <113038235+MaheswariYalini@users.noreply.github.com>
2022-10-05 12:34:29 -0400

    Create PushTrigger.java

---
+++ PushTrigger.java
@@ -0,0 +1,8 @@
+public class JenkinsGit{
+
+public static void main(String[] args){
+
+System.out.println("Check integration of GIT and Jenkins");
+System.out.println("Check Build is triggered after the GIT Commit");
+}
+}
